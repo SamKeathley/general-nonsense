192274a988326f66437388972a14e0b80aea7b5d
Sam Keathley <samncollette@gmail.com>
2020-11-24 11:28:10 -0800

    implement product search in the header

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,20 +1,22 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col } from 'react-bootstrap';
-import Product from '../components/Product';
-import Message from '../components/Message';
-import Loader from '../components/Loader';
-import { listProducts } from '../actions/productActions.js';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProducts } from '../actions/productActions.js'
 
-const HomeScreen = () => {
-  const dispatch = useDispatch();
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword
 
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+  const dispatch = useDispatch()
+
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    dispatch(listProducts());
-  }, [dispatch]);
+    dispatch(listProducts(keyword))
+  }, [dispatch, keyword])
 
   return (
     <>
@@ -33,7 +35,7 @@ const HomeScreen = () => {
         </Row>
       )}
     </>
-  );
-};
+  )
+}
 
-export default HomeScreen;
+export default HomeScreen
